5b70ca49d848951b4b09dc9587e98eae33ee0f96
hugosique <otahugo02@gmail.com>
2023-07-25 17:23:18 -0300

    API integration

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 
 // Observable
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { tap, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -19,9 +19,21 @@ export class PokeApiService {
   get apiListAllPokemons(): Observable<any> {
     return this.http.get(this.url).pipe(
       tap(res => res),
-      tap(res => {
-        console.log(res)
-      })
+      tap((res: any) => {
+        res.results.map((resPokemons: any) => {
+          this.getPokemons(resPokemons.url).subscribe(
+            res => resPokemons.status = res
+          )
+        });
+      }),
+    );
+  }
+
+  public getPokemons(url: string): Observable<any> {
+    return this.http.get(url).pipe(
+      map(
+        res => res
+      )
     );
   }
 }
