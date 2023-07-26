2e4185f3ac01dd3866b91691c421e6dedf635299
hugosique <otahugo02@gmail.com>
2023-07-26 01:18:17 -0300

    Pokedex working

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { PokeApiService } from './../../service/poke-api.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  public pokemon: any
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService,
+  ) {}
+
+  ngOnInit(): void {
+    this.getPoke;
+  }
+  get getPoke() {
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.getPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.getPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe(
+      (res: any) => {
+        this.pokemon = res;
+      }
+    )
+  }
 
 }
